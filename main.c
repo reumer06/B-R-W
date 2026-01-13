9e0e8272fe5b56d631c9b65384634a4b5330e145
Umer Shaikh <n0sync.76@gmail.com>
2026-01-14 01:12:10 +0530

    feat: add boundary checks
    Added probability calculation for attacker success based on variable 'z'.

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <math.h>
+#include <stdio.h>
 
 double attackersuccessprobability(double q,int z) 
-{ 
+{
+    if (q >= 0.5) return 1.0; // If attacker has 50% or more,he will eventually win
+    if (q <= 0.0) return 0.0;
+    
     double p = 1.0 - q;     // power of honest node
     double lambda = (double)z * (q/p); // expected number of attacks, mean of poisson distribution
     double sum = 1.0;   // complementary probability for the attacker
@@ -13,7 +17,13 @@ double attackersuccessprobability(double q,int z)
     return sum;
 }
 
-int main()
+
+int main() 
 {
-    attackersuccessprobability(0.4,1);
+    double q = 0.3;
+    printf("Attacker success probability (q=%.2f):\n",q);
+
+    for(int z = 0;z <=10;++z){
+        printf("z= %2d: Probability = %f\n",z,attackersuccessprobability(q,z));
+    }
 }
